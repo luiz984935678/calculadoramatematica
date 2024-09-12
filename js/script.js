efae989e8f0e062612d9a0bf500fe195db4beebf
luiz <luizoctaviosouza2008@gmail.com>
2024-09-12 19:55:41 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-  
+$(document).ready(function() {
+    $('.bt-nr').click(function(){
+        var bt_nr = $(this).text();
+
+        $('#txt_dispaly').val(bt_nr);
+    });
+
 });
     
-
